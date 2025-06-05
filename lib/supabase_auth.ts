4baeb2f3224c598ac64aafe9ab448691c25ae205
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-06-04 23:36:58 -0600

    Changed how the functions defined in `lib/supabase_auth.ts` work
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lib/supabase_auth.ts
+++ lib/supabase_auth.ts
@@ -4,19 +4,22 @@ export async function signUp({ email, password }: { email: string; password: str
 	const { data, error } = await supabase.auth.signUp({ email, password });
 
 	if (error)
-		console.error("Sign Up Page - Error signing up new user:", error.message);
+		console.error('Sign Up - Error signing up new user:' + error.message);
 	else
-		console.log("Sign Up Page - User signed up successfully: ", data);
+		console.log('Sign Page - User signed up successfully:' + data);
 
-	return data;
+	return { ...data, error };
 }
 
-export async function signIn({ email, password }: { email: string; password: string }) {
+export async function login({ email, password }: { email: string; password: string }) {
 	const { data, error } = await supabase.auth.signInWithPassword({ email, password });
 
-	if (error) throw error;
+	if (error)
+    console.error('Login - Error signing in user:' + error.message);
+  else
+    console.log('Login - User signed in successfully:' + data);
 
-	return data;
+	return { ...data, error };
 }
 
 export async function signOut() {
