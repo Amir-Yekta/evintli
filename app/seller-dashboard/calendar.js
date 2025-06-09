d7c5b2cc0beda1b706ac903ca14f4f95290937bd
Amir <amirhosseinfarahaniyekta@gmail.com>
2025-06-09 14:29:09 -0600

    update

--- app/seller-dashboard/calendar.js
+++
@@ -1,9 +0,0 @@
-/*This gets rendered to the page.js of the seller-dashboard*/
-
-export default function CalendarSection() {
-    return(
-        <div>
-            <p className="font-black">This is A calendar</p>
-        </div>
-    );
-}
